b676b1a2a628ea3ed3e0e9b603537123b3d84fe5
Xpg <143957988+Xproggame@users.noreply.github.com>
2026-09-25 12:00:28 +0200

    Ajout de la transformation des chaines de caractere et transformation des nombres entiers.

--- Identifiant/Variable.py
+++ Identifiant/Variable.py
@@ -6,6 +6,23 @@ class Variable:
     def creer(self, identifiant:str, valeur:tuple):
 
         if valeur[1] == 'chaine de caractere':
-            valeurfinal = valeur
+            valeurfinale = (
+                self.Traiter.chaine_de_caractere(valeur[0]),
+                valeur[1]
+                )
 
-        if valeur
+        if valeur[1] == 'nombre entier':
+            valeurfinale = (
+                    self.Traiter.nombre_entier(valeur[0]),
+                    valeur[1]
+                    )
+
+    class Traiter:
+
+	    @staticmethod
+
+	    def chaine_de_caractere(chaine:str):
+		    return chaine[1:len(chaine) - 1]
+		   
+		def nombre_entier(valeur:str):
+		    return int(valeur)
